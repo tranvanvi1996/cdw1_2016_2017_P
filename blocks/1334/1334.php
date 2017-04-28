83d77d4860d76f016c18847251fe201a2ebf1020
HoanNguyenIM <nguyenanhhoanld@gmail.com>
2017-04-28 07:23:49 +0700

    Vi Tran

---
+++ blocks/1334/1334.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+
+    <?php
+        if (!class_exists('lessc')) {
+            include ('./libs/lessc.inc.php');
+        }
+        $less = new lessc;
+        $less->compileFile('less/1334.less', 'css/1334.css');
+        ?>
+        <link href="css/1334.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body>
+
+    <div class="type-1334">
+        <div class="margin"></div>
+        <div class="ornament-top"></div>
+        <div class="introduction">
+            <div class="detail">
+                <div class="sep-text single centered">
+                    <img src="images/pho.jpg" alt="" />
+                    <div class="sep-text-before">
+                        <div class="sep-text-line"></div>
+                    </div>
+                    <div class="content">
+                        <h2 class="text-divider-double">
+                                Brunch
+                            </h2>
+                        <div class="text-divider-subtitle">Starts at 11:00 am</div>
+                    </div>
+                    <div class="sep-text-after">
+                        <div class="sep-text-line"></div>
+                    </div>
+                </div>
+                <div class="fp_box">
+                    <h3 class="" title="American Brunch Combo Menu">American Brunch Combo Menu<span class="fp_price">$ 15</span></h3>
+                    <div class="menu_description">Freshly scrambled eggs with applewood smoked bacon, tomatoes, green onions and cheddar cheese.</div>
+                </div>
+                <div class="fp_box">
+                    <h3 class="" title="American Brunch Combo Menu">American Brunch Combo Menu<span class="fp_price">$ 15</span></h3>
+                    <div class="menu_description">Freshly scrambled eggs with applewood smoked bacon, tomatoes, green onions and cheddar cheese.</div>
+                </div>
+                <div class="fp_box">
+                    <h3 class="" title="American Brunch Combo Menu">American Brunch Combo Menu<span class="fp_price">$ 15</span></h3>
+                    <div class="menu_description">Freshly scrambled eggs with applewood smoked bacon, tomatoes, green onions and cheddar cheese.</div>
+                </div>
+                <div class="fp_box">
+                    <h3 class="" title="American Brunch Combo Menu">American Brunch Combo Menu<span class="fp_price">$ 15</span></h3>
+                    <div class="menu_description">Freshly scrambled eggs with applewood smoked bacon, tomatoes, green onions and cheddar cheese.</div>
+                </div>
+                <div class="fp_box">
+                    <h3 class="" title="American Brunch Combo Menu">American Brunch Combo Menu<span class="fp_price">$ 15</span></h3>
+                    <div class="menu_description">Freshly scrambled eggs with applewood smoked bacon, tomatoes, green onions and cheddar cheese.</div>
+                </div>
+            </div>
+        </div>
+        <div class="ornament-bottom"></div>
+        <div class="margin"></div>
+    </div>
+</body>
+
+</html>
